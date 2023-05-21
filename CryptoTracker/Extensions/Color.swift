78ac0c58bb7b36eb112d6b80443b9139098c3b0c
shagar123 <shagar@appadore.com>
2023-05-21 20:25:31 +0530

    configured colors

---
+++ CryptoTracker/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by shagar p.k on 21/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
